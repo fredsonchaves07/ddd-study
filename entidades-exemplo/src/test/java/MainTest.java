64e71517914897ae83aedccca37fbc9d3d40460d
Fredson <fredsonchaves07@gmail.com>
2023-05-25 10:11:02 -0300

    feat: exemplo de classe anemica

---
+++ entidades-exemplo/src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import com.fredsonchaves07.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void hello() {
+        Main.main(null);
+        assertTrue(true);
+    }
+}
